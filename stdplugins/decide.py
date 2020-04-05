7603971cca1bb4eb0308af381ec0ef322827770b
BeastBot <52532536+BeastBot@users.noreply.github.com>
2020-04-05 21:53:44 +0530

    Update decide.py

--- stdplugins/decide.py
+++ stdplugins/decide.py
@@ -5,7 +5,7 @@ import requests
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd("decide"))
+@borg.on(admin_cmd(pattern="decide", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
